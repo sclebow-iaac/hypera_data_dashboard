c0fed669b8634b9d8846d7ea5273c192e322a1cf
sclebow-iaac <scott.lebow@students.iaac.net>
2025-03-03 13:15:36 +0000

    Update model name in residential extractor for accurate data sourcing

--- front_end/data_extraction/residential_extractor.py
+++ front_end/data_extraction/residential_extractor.py
@@ -2,7 +2,7 @@
 
 import data_extraction.data_extractor as data_extractor
 
-model_name = "hypera/residential/programming/residential/data"
+model_name = "hypera/residential/building form/residential"
 
 data_names = [
     "ListOfUnitFunctions",
