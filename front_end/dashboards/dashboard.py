71fd5a53e699451ff7de70eda8047611fcdb78cf
sclebow-iaac <scott.lebow@students.iaac.net>
2025-03-24 22:34:36 -0400

    feat: Refactor speckle viewer functionality and update presentation model ID

--- front_end/dashboards/dashboard.py
+++ front_end/dashboards/dashboard.py
@@ -12,6 +12,8 @@ import math
 
 from browser_detection import browser_detection_engine
 
+import viewer
+
 content_container_width = 8  # Adjust this value to set the width of the content container
 
 class Metric:
@@ -241,37 +243,17 @@ def setup_speckle_connection(models_limit=100):
     return models, client, project_id
 
 def display_speckle_viewer(container, project_id, model_id, is_transparent=False, hide_controls=False, hide_selection_info=False, no_scroll=False, height=400, include_site=False, header_text='Representational Model'):
-    container.markdown(f'#### {header_text}')
-
-    speckle_model_url = f'https://macad.speckle.xyz/projects/{project_id}/models/{model_id}'
-
-    if include_site:
-        speckle_model_url += ',693847dff2'
-
-    # https://macad.speckle.xyz/projects/31f8cca4e0/models/e76ccf2e0f,3f178d9658,a4e3d78009,c710b396d3,5512057f5b,d68a58c12d,2b48d3f757,767672f412
-    # speckle_model_url += '#embed={%22isEnabled%22:true,%22isTransparent%22:true,%22hideControls%22:true,%22hideSelectionInfo%22:true,%22noScroll%22:true}'
-
-    embed_str = '%22isEnabled%22:true'
-    if is_transparent:
-        embed_str += ',%22isTransparent%22:true'
-    if hide_controls:
-        embed_str += ',%22hideControls%22:true'
-    if hide_selection_info:
-        embed_str += ',%22hideSelectionInfo%22:true'
-    if no_scroll:
-        embed_str += ',%22noScroll%22:true'
-
-    speckle_model_url += f'#embed={{{embed_str}}}'
-
-    iframe_code = f"""
-        <iframe src="{speckle_model_url}"
-                style="width: 100%; height: {height}px; border: none;">
-        </iframe>
-        """
-    container.markdown(iframe_code, unsafe_allow_html=True)
-
-    return speckle_model_url
-
+    return viewer.display_speckle_viewer(
+        container, project_id, model_id,
+        is_transparent=is_transparent,
+        hide_controls=hide_controls,
+        hide_selection_info=hide_selection_info,
+        no_scroll=no_scroll,
+        height=height,
+        include_site=include_site,
+        header_text=header_text
+    )
+    
 def display_page_title(team_name: str) -> None:
     st.markdown(f'''
         <div style="text-align: center;">
--- front_end/viewer.py
+++ front_end/viewer.py
@@ -1,25 +1,31 @@
-def display_speckle_viewer(container, project_id, model_id, is_transparent=False, hide_controls=False, hide_selection_info=False, no_scroll=False):
-        speckle_model_url = f'https://macad.speckle.xyz/projects/{project_id}/models/{model_id}'
-        # https://macad.speckle.xyz/projects/31f8cca4e0/models/e76ccf2e0f,3f178d9658,a4e3d78009,c710b396d3,5512057f5b,d68a58c12d,2b48d3f757,767672f412
-        # speckle_model_url += '#embed={%22isEnabled%22:true,%22isTransparent%22:true,%22hideControls%22:true,%22hideSelectionInfo%22:true,%22noScroll%22:true}'
+def display_speckle_viewer(container, project_id, model_id, is_transparent=False, hide_controls=False, hide_selection_info=False, no_scroll=False, height=400, include_site=False, header_text='Representational Model'):
+    container.markdown(f'#### {header_text}')
 
-        embed_str = '%22isEnabled%22:true'
-        if is_transparent:
-            embed_str += ',%22isTransparent%22:true'
-        if hide_controls:
-            embed_str += ',%22hideControls%22:true'
-        if hide_selection_info:
-            embed_str += ',%22hideSelectionInfo%22:true'
-        if no_scroll:
-            embed_str += ',%22noScroll%22:true'
+    speckle_model_url = f'https://macad.speckle.xyz/projects/{project_id}/models/{model_id}'
 
-        speckle_model_url += f'#embed={{{embed_str}}}'
+    if include_site:
+        speckle_model_url += ',693847dff2'
 
-        iframe_code = f"""
+    # https://macad.speckle.xyz/projects/31f8cca4e0/models/e76ccf2e0f,3f178d9658,a4e3d78009,c710b396d3,5512057f5b,d68a58c12d,2b48d3f757,767672f412
+    # speckle_model_url += '#embed={%22isEnabled%22:true,%22isTransparent%22:true,%22hideControls%22:true,%22hideSelectionInfo%22:true,%22noScroll%22:true}'
+
+    embed_str = '%22isEnabled%22:true'
+    if is_transparent:
+        embed_str += ',%22isTransparent%22:true'
+    if hide_controls:
+        embed_str += ',%22hideControls%22:true'
+    if hide_selection_info:
+        embed_str += ',%22hideSelectionInfo%22:true'
+    if no_scroll:
+        embed_str += ',%22noScroll%22:true'
+
+    speckle_model_url += f'#embed={{{embed_str}}}'
+
+    iframe_code = f"""
         <iframe src="{speckle_model_url}"
-                style="width: 100%; height: 600px; border: none;">
+                style="width: 100%; height: {height}px; border: none;">
         </iframe>
         """
-        container.markdown(iframe_code, unsafe_allow_html=True)
+    container.markdown(iframe_code, unsafe_allow_html=True)
 
-        return speckle_model_url
+    return speckle_model_url
